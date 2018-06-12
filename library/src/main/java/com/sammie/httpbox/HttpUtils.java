ee4efb783cc688c19bdc7d34b6573c02720b1b93
zhangweijun <wj3519@qq.com>
2018-06-12 17:19:50 +0800

    初次提交代码

--- library/src/main/java/com/sammie/httpbox/HttpUtils.java
+++ library/src/main/java/com/sammie/httpbox/HttpUtils.java
@@ -1,5 +1,8 @@
 package com.sammie.httpbox;
 
+import android.content.Context;
+import android.view.View;
+
 /**
 * Http请求入口
 * @author ZhangWeijun
@@ -9,22 +12,18 @@ public class HttpUtils {
 
     // 这个可以在 application 中去初始化
     private static IHttpRequest mInitHttpRequest;
-    private IHttpRequest mHttpRequest;
-    public static void initHttpRequest(IHttpRequest httpRequest) {
+    private static HttpConfig mHttpConfig;
+
+    public static void initHttpRequest(IHttpRequest httpRequest,HttpConfig httpConfig) {
         mInitHttpRequest = httpRequest;
+        mHttpConfig = httpConfig;
     }
-    // 如果有两种的情况下 比如 volley 下载文件并不是很屌 ，那么可以换成 OKHttp
-    public HttpUtils httpRequest(IHttpRequest httpRequest) {
-        this.mHttpRequest = httpRequest;
-        return this;
-    }
+
+
     // 省略部分代码 ......
     public <T> void execute(IHttpCallBack<T> callback) {
         // 如果没有指定，那么就用 application 中初始化的
-        if(mHttpRequest == null){
-            mHttpRequest = mInitHttpRequest;
-        }
-//        mHttpRequest.get(mContext, mParams, mUrl, mCache, callback);
+//        mInitHttpRequest.get(mContext, mParams, mUrl, mCache, callback);
     }
 
 }
